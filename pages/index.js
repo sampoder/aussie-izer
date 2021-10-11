c4ac70af298ad8f13a21e46eba08e24d3e78fa01
Sam Poder <39828164+sampoder@users.noreply.github.com>
2021-10-11 13:42:45 +0800

    Clean up!

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,45 @@
 import Head from "next/head";
 
 export default function Home(props) {
-  switch(props.wildcard) {
+  switch (props.wildcard) {
     case "home":
-      return <div>Welcome to the home page!</div>;
+      return (
+        <div>
+          Welcome to the Aussie-izer! This only works for .com domains. If you
+          want to Aussie-ize{" "}
+          <a href="https://example.com">https://example.com</a> visit{" "}
+          <a href="https://example.aussieizer.sampoder.com">
+            https://example.aussieizer.sampoder.com
+          </a>
+          .{" "}
+        </div>
+      );
       break;
     default:
-      return <iframe src={`https://${props.wildcard}.com`} style={{transform: 'rotate(180deg)', border: 'none', height: '100vh', width: '100%', overflow: 'hidden'}} frameBorder="0" scrolling="yes" seamless="seamless" height="100%" width="100%"></iframe>;
+      return (
+        <iframe
+          src={`https://${props.wildcard}.com`}
+          style={{
+            transform: "rotate(180deg)",
+            border: "none",
+            height: "100vh",
+            width: "100%",
+            overflow: "hidden",
+          }}
+          frameBorder="0"
+          scrolling="yes"
+          seamless="seamless"
+          height="100%"
+          width="100%"
+        ></iframe>
+      );
   }
 }
 
 export async function getServerSideProps(context) {
   let wildcard = context.req.headers.host.split(".")[0];
   wildcard =
-    wildcard != "wildcardtest" && wildcard != "is-a-maker"
+    wildcard != "aussieizer"
       ? wildcard != "localhost:3000"
         ? wildcard
         : process.env.TEST_WILDCARD
